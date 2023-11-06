09790b01f8ea58dc54c1db7bbfb4aa978bd73373
Gustavo Tula <tula.gustavo.contacto@gmail.com>
2023-11-06 11:37:33 -0300

    finalizado

--- main.js
+++ main.js
@@ -1,79 +1,62 @@
-function editarDatosUsuario() {
-    alert("Volvamos a editar los datos ingresados.");
-    nombre = prompt("Ingrese su nombre");
-    edad = prompt("Ingrese su edad");
-    dni = prompt("Ingrese su DNI");
-    datosUsuario = "Nombre ingresado: " + nombre + "\nEdad: " + edad + "\nDNI: " + dni;
-    alert(datosUsuario);
-}
+document.addEventListener('DOMContentLoaded', function() {
+    const userInputDiv = document.getElementById('userInput');
+    const destinosDiv = document.getElementById('destinos');
+    const resultadoDiv = document.getElementById('resultado');
+    const confirmarDatosButton = document.getElementById('confirmarDatos');
+    const confirmarDestinoButton = document.getElementById('confirmarDestino');
+    const nombreInput = document.getElementById('nombre');
+    const edadInput = document.getElementById('edad'); 
+    const dniInput = document.getElementById('dni');
+    const destinosList = document.querySelectorAll('#destinosList li');
+    let usuario = null;
+    let destinoElegido = null;
 
-alert("Bienvenidos al sistema de viaje CAMBA!");
-alert("Puede elegir su destino favorito dentro de la Argentina teniendo en cuenta sus gustos!");
+    const usuarioGuardado = localStorage.getItem('usuario');
+    if (usuarioGuardado) {
+        usuario = JSON.parse(usuarioGuardado);
+        userInputDiv.style.display = 'none';
+        destinosDiv.style.display = 'block';
+    }
 
-let usuario = {
-    nombre: "",
-    edad: 0,
-    dni: ""
-};
+    confirmarDatosButton.addEventListener('click', function() {
+        const nombre = nombreInput.value;
+        const edad = parseInt(edadInput.value); 
+        const dni = dniInput.value;
+        
+        if (nombre && !isNaN(edad) && dni) {
+            usuario = { nombre, edad, dni };
+            userInputDiv.style.display = 'none';
+            destinosDiv.style.display = 'block';
 
-usuario.nombre = prompt("Ingrese su nombre");
-usuario.edad = parseInt(prompt("Ingrese su edad")); 
-usuario.dni = prompt("Ingrese su DNI");
-
-let datosUsuario = "Nombre ingresado: " + usuario.nombre + "\nEdad: " + usuario.edad + "\nDNI: " + usuario.dni;
-alert(datosUsuario);
-
-let editarDatos = prompt("¿Está conforme con los datos ingresados?\n1 - Sí\n2 - No");
-
-if (editarDatos === "2") {
-    editarDatosUsuario();
-}
-
-alert("Sigamos con su elección de provincias que desea recorrer");
-alert("1: Mendoza - $40,000");
-alert("2: Buenos Aires - $50,000");
-alert("3: Córdoba - $35,000");
-alert("4: Santa Cruz - $70,000");
-
-let destinos = [
-    { nombre: "Mendoza", precio: 40000 },
-    { nombre: "Buenos Aires", precio: 50000 },
-    { nombre: "Córdoba", precio: 35000 },
-    { nombre: "Santa Cruz", precio: 70000 }
-];
-
-let opcionDestino = prompt("Ingrese el número del destino que desea visitar");
-
-
-function buscarDestinoPorNombre(nombre) {
-    return destinos.find(destino => destino.nombre === nombre);
-}
-
-switch (opcionDestino) {
-    case "1":
-        let destinoMendoza = buscarDestinoPorNombre("Mendoza");
-        if (destinoMendoza) {
-            alert(usuario.nombre + ", lo esperamos en " + destinoMendoza.nombre + " por un total de: $" + destinoMendoza.precio);
+            sessionStorage.setItem('usuario', JSON.stringify(usuario));
+        } else {
+            alert("Por favor, complete todos los campos correctamente.");
         }
-        break;
-    case "2":
-        let destinoBuenosAires = buscarDestinoPorNombre("Buenos Aires");
-        if (destinoBuenosAires) {
-            alert(usuario.nombre + ", lo esperamos en " + destinoBuenosAires.nombre + " por un total de: $" + destinoBuenosAires.precio);
+    });
+
+    destinosList.forEach(destino => {
+        destino.addEventListener('click', function() {
+            destinoElegido = destino.getAttribute('data-destino');
+            confirmarDestinoButton.style.display = 'block';
+        });
+    });
+
+    confirmarDestinoButton.addEventListener('click', function() {
+        if (destinoElegido) {
+            const datosUsuario = `Nombre ingresado: ${usuario.nombre}\nEdad: ${usuario.edad}\nDNI: ${usuario.dni}`;
+            const mensaje = `Información del usuario:\n\n${datosUsuario}\nDestino elegido: ${destinoElegido}`;
+            
+            Swal.fire({
+                title: 'Información del Usuario y Destino',
+                text: mensaje,
+                icon: 'info',
+                confirmButtonText: 'Aceptar'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    location.reload();
+                }
+            });
         }
-        break;
-    case "3":
-        let destinoCordoba = buscarDestinoPorNombre("Córdoba");
-        if (destinoCordoba) {
-            alert(usuario.nombre + ", lo esperamos en " + destinoCordoba.nombre + " por un total de: $" + destinoCordoba.precio);
-        }
-        break;
-    case "4":
-        let destinoSantaCruz = buscarDestinoPorNombre("Santa Cruz");
-        if (destinoSantaCruz) {
-            alert(usuario.nombre + ", lo esperamos en " + destinoSantaCruz.nombre + " por un total de: $" + destinoSantaCruz.precio);
-        }
-        break;
-    default:
-        alert("Opción no válida. Hasta la próxima.");
-}
+    });
+
+});
